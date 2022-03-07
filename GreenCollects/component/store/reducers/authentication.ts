939a0868221e883798af3799c5646036429edccb
baretd <dorian.baret@etu.univ-grenoble-alpes.fr>
2022-03-07 12:17:54 +0100

    feat: navigation, affichage des infos du user

--- GreenCollects/component/store/reducers/authentication.ts
+++ GreenCollects/component/store/reducers/authentication.ts
@@ -1,12 +1,13 @@
-import { DefaultRootState, useDispatch } from "react-redux";
-import { combineReducers } from "redux";
-import { LOGIN, LOGOUT } from "../types";
-
-const AUTH_TOKEN_KEY = 'greencollect-authenticationToken';
+import { LOGIN, LOGOUT, PROFIL } from "../types";
 
 const INITIAL_STATE = {
     token: '',
-    username: '',
+    user: {
+        username: '',
+        email: '',
+        first_name: '',
+        last_name: ''
+    }
 };
 
 const authenticationReducer = (state: any = INITIAL_STATE, action: any) => {
@@ -15,11 +16,19 @@ const authenticationReducer = (state: any = INITIAL_STATE, action: any) => {
             var newState: any = {
                 ...state,
                 token: action.value.token,
-                username: action.value.username
+                user: {
+                    username: action.value.username
+                }
             }
             return newState;
         case LOGOUT:
             var newState: any = INITIAL_STATE
+            return newState;
+        case PROFIL:
+            var newState: any = {
+                ...state,
+                user: action.value.user
+            }
             return newState;
         default:
             return state
