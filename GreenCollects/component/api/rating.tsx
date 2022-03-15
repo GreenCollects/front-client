a683332cddcaeabc4561f33a4ffa2cb745028084
Malone <malone.julienne@gmail.com>
2022-03-15 14:05:04 +0100

    fix: changement du code pour le système de vote

--- GreenCollects/component/api/rating.tsx
+++ GreenCollects/component/api/rating.tsx
@@ -7,19 +7,30 @@ import HTTP_MAP_ERROR_CODE from "./errors";
 
 const API_POINT_URL = "/api/rating/";
 
-export const getRateAvarage = async (headers: Headers, idPoint : number) => {
-    const url = `${env.SERVER_API_URL}${API_POINT_URL}point/${idPoint}`;
-    const brut_response = await fetch(
-        url,
-        {
-            method: "GET",
-            headers: headers
-        }
-    ).then((response) => {
-        if (response.ok) {
-            return response.json();
-        } else {
-            Toast.show(HTTP_MAP_ERROR_CODE[response.status], {
+export const getRateAvarage = async (headers: Headers, idPoint: number) => {
+    const url = `${env.SERVER_API_URL}${API_POINT_URL}point/${idPoint}/`;
+    const brut_response = await fetch(url, {
+        method: "GET",
+        headers: headers,
+    })
+        .then((response) => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                Toast.show(HTTP_MAP_ERROR_CODE[response.status], {
+                    duration: Toast.durations.SHORT,
+                    position: Toast.positions.BOTTOM,
+                    shadow: true,
+                    animation: true,
+                    hideOnPress: true,
+                    delay: 0,
+                    backgroundColor: "#f13e71",
+                    textColor: "#fff",
+                });
+            }
+        })
+        .catch((err) => {
+            Toast.show(HTTP_MAP_ERROR_CODE[503], {
                 duration: Toast.durations.SHORT,
                 position: Toast.positions.BOTTOM,
                 shadow: true,
@@ -29,38 +40,40 @@ export const getRateAvarage = async (headers: Headers, idPoint : number) => {
                 backgroundColor: "#f13e71",
                 textColor: "#fff",
             });
-        }
-    }).catch((err) => {
-        Toast.show(HTTP_MAP_ERROR_CODE[503], {
-            duration: Toast.durations.SHORT,
-            position: Toast.positions.BOTTOM,
-            shadow: true,
-            animation: true,
-            hideOnPress: true,
-            delay: 0,
-            backgroundColor: "#f13e71",
-            textColor: "#fff",
         });
-    });
 
     return brut_response;
-}
+};
 
-export const getRatingForCurrentUser = async (headers: Headers, idPoint : number) => {
-    const url = `${env.SERVER_API_URL}${API_POINT_URL}point/${idPoint}/user`;
-    const brut_response = await fetch(
-        url,
-        {
-            method: "GET",
-            headers: headers
-        }
-    ).then((response) => {
-        if (response.status === 200) {
-            return response.json();
-        } else if (response.status === 204) {
-            return undefined;
-        } else {
-            Toast.show(HTTP_MAP_ERROR_CODE[response.status], {
+export const getRatingForCurrentUser = async (
+    headers: Headers,
+    idPoint: number
+) => {
+    const url = `${env.SERVER_API_URL}${API_POINT_URL}point/${idPoint}/user/`;
+    const brut_response = await fetch(url, {
+        method: "GET",
+        headers: headers,
+    })
+        .then((response) => {
+            if (response.status === 200) {
+                return response.json();
+            } else if (response.status === 204) {
+                return undefined;
+            } else {
+                Toast.show(HTTP_MAP_ERROR_CODE[response.status], {
+                    duration: Toast.durations.SHORT,
+                    position: Toast.positions.BOTTOM,
+                    shadow: true,
+                    animation: true,
+                    hideOnPress: true,
+                    delay: 0,
+                    backgroundColor: "#f13e71",
+                    textColor: "#fff",
+                });
+            }
+        })
+        .catch((err) => {
+            Toast.show(HTTP_MAP_ERROR_CODE[503], {
                 duration: Toast.durations.SHORT,
                 position: Toast.positions.BOTTOM,
                 shadow: true,
@@ -70,37 +83,36 @@ export const getRatingForCurrentUser = async (headers: Headers, idPoint : number
                 backgroundColor: "#f13e71",
                 textColor: "#fff",
             });
-        }
-    }).catch((err) => {
-        Toast.show(HTTP_MAP_ERROR_CODE[503], {
-            duration: Toast.durations.SHORT,
-            position: Toast.positions.BOTTOM,
-            shadow: true,
-            animation: true,
-            hideOnPress: true,
-            delay: 0,
-            backgroundColor: "#f13e71",
-            textColor: "#fff",
         });
-    });
 
     return brut_response;
-}
+};
 
 export const setRating = async (headers: Headers, body: any) => {
     const url = `${env.SERVER_API_URL}${API_POINT_URL}`;
-    const brut_response = await fetch(
-        url,
-        {
-            method: "POST",
-            headers: headers,
-            body: JSON.stringify(body),
-        }
-    ).then((response) => {
-        if (response.ok) {
-            return response.json();
-        } else {
-            Toast.show(HTTP_MAP_ERROR_CODE[response.status], {
+    const brut_response = await fetch(url, {
+        method: "POST",
+        headers: headers,
+        body: body,
+    })
+        .then((response) => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                Toast.show(HTTP_MAP_ERROR_CODE[response.status], {
+                    duration: Toast.durations.SHORT,
+                    position: Toast.positions.BOTTOM,
+                    shadow: true,
+                    animation: true,
+                    hideOnPress: true,
+                    delay: 0,
+                    backgroundColor: "#f13e71",
+                    textColor: "#fff",
+                });
+            }
+        })
+        .catch((err) => {
+            Toast.show(HTTP_MAP_ERROR_CODE[503], {
                 duration: Toast.durations.SHORT,
                 position: Toast.positions.BOTTOM,
                 shadow: true,
@@ -110,37 +122,36 @@ export const setRating = async (headers: Headers, body: any) => {
                 backgroundColor: "#f13e71",
                 textColor: "#fff",
             });
-        }
-    }).catch((err) => {
-        Toast.show(HTTP_MAP_ERROR_CODE[503], {
-            duration: Toast.durations.SHORT,
-            position: Toast.positions.BOTTOM,
-            shadow: true,
-            animation: true,
-            hideOnPress: true,
-            delay: 0,
-            backgroundColor: "#f13e71",
-            textColor: "#fff",
         });
-    });
 
     return brut_response;
-}
+};
 
 export const changeRating = async (headers: Headers, id: number, body: any) => {
     const url = `${env.SERVER_API_URL}${API_POINT_URL}${id}/`;
-    const brut_response = await fetch(
-        url,
-        {
-            method: "PUT",
-            headers: headers,
-            body: JSON.stringify(body),
-        }
-    ).then((response) => {
-        if (response.ok) {
-            return response.json();
-        } else {
-            Toast.show(HTTP_MAP_ERROR_CODE[response.status], {
+    const brut_response = await fetch(url, {
+        method: "PUT",
+        headers: headers,
+        body: body,
+    })
+        .then((response) => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                Toast.show(HTTP_MAP_ERROR_CODE[response.status], {
+                    duration: Toast.durations.SHORT,
+                    position: Toast.positions.BOTTOM,
+                    shadow: true,
+                    animation: true,
+                    hideOnPress: true,
+                    delay: 0,
+                    backgroundColor: "#f13e71",
+                    textColor: "#fff",
+                });
+            }
+        })
+        .catch((err) => {
+            Toast.show(HTTP_MAP_ERROR_CODE[503], {
                 duration: Toast.durations.SHORT,
                 position: Toast.positions.BOTTOM,
                 shadow: true,
@@ -150,36 +161,35 @@ export const changeRating = async (headers: Headers, id: number, body: any) => {
                 backgroundColor: "#f13e71",
                 textColor: "#fff",
             });
-        }
-    }).catch((err) => {
-        Toast.show(HTTP_MAP_ERROR_CODE[503], {
-            duration: Toast.durations.SHORT,
-            position: Toast.positions.BOTTOM,
-            shadow: true,
-            animation: true,
-            hideOnPress: true,
-            delay: 0,
-            backgroundColor: "#f13e71",
-            textColor: "#fff",
         });
-    });
 
     return brut_response;
-}
+};
 
 export const deleteRating = async (headers: Headers, id: number) => {
     const url = `${env.SERVER_API_URL}${API_POINT_URL}${id}/`;
-    const brut_response = await fetch(
-        url,
-        {
-            method: "DELETE",
-            headers: headers,
-        }
-    ).then((response) => {
-        if (response.ok) {
-            return response.json();
-        } else {
-            Toast.show(HTTP_MAP_ERROR_CODE[response.status], {
+    const brut_response = await fetch(url, {
+        method: "DELETE",
+        headers: headers,
+    })
+        .then((response) => {
+            if (response.ok) {
+                return {};
+            } else {
+                Toast.show(HTTP_MAP_ERROR_CODE[response.status], {
+                    duration: Toast.durations.SHORT,
+                    position: Toast.positions.BOTTOM,
+                    shadow: true,
+                    animation: true,
+                    hideOnPress: true,
+                    delay: 0,
+                    backgroundColor: "#f13e71",
+                    textColor: "#fff",
+                });
+            }
+        })
+        .catch((err) => {
+            Toast.show(HTTP_MAP_ERROR_CODE[503], {
                 duration: Toast.durations.SHORT,
                 position: Toast.positions.BOTTOM,
                 shadow: true,
@@ -189,19 +199,7 @@ export const deleteRating = async (headers: Headers, id: number) => {
                 backgroundColor: "#f13e71",
                 textColor: "#fff",
             });
-        }
-    }).catch((err) => {
-        Toast.show(HTTP_MAP_ERROR_CODE[503], {
-            duration: Toast.durations.SHORT,
-            position: Toast.positions.BOTTOM,
-            shadow: true,
-            animation: true,
-            hideOnPress: true,
-            delay: 0,
-            backgroundColor: "#f13e71",
-            textColor: "#fff",
         });
-    });
 
     return brut_response;
-}
+};
--- GreenCollects/component/map-screens/CardDetails.tsx
+++ GreenCollects/component/map-screens/CardDetails.tsx
@@ -3,10 +3,22 @@ import { Button, Card, Text, Spinner, Layout } from "@ui-kitten/components";
 import { StyleSheet, View } from "react-native";
 import tw from "twrnc";
 
-import { CrossIcon, ArrowDownIcon, ArrowDownFillIcon, ArrowUpFillIcon, ArrowUpIcon } from "../icons/icons";
+import {
+    CrossIcon,
+    ArrowDownIcon,
+    ArrowDownFillIcon,
+    ArrowUpFillIcon,
+    ArrowUpIcon,
+} from "../icons/icons";
 import { RootState } from "../store/Store";
 import { useSelector } from "react-redux";
-import { getRateAvarage, changeRating, getRatingForCurrentUser, deleteRating, setRating } from "../api/rating";
+import {
+    getRateAvarage,
+    changeRating,
+    getRatingForCurrentUser,
+    deleteRating,
+    setRating,
+} from "../api/rating";
 
 export const nameOrAddress = (address: any) => {
     if (address.name !== address.subThoroughfare) {
@@ -23,13 +35,23 @@ type PropsType = {
     deselect?: () => void;
 };
 
-const CardDetails: FC<PropsType> = ({marker, address, wasteLabels, deselect}) => {
-    const authentication = useSelector((state: RootState) => state.authentication);
+const CardDetails: FC<PropsType> = ({
+    marker,
+    address,
+    wasteLabels,
+    deselect,
+}) => {
+    let now = Date.now();
+    const DELAY = 300;
+
+    const authentication = useSelector(
+        (state: RootState) => state.authentication
+    );
 
     const [infoRating, setInfoRating] = useState({
-        "idPoint": 0,
-        "rate": 0,
-        "denominator": 10
+        idPoint: 0,
+        rate: 0,
+        denominator: 10,
     });
     const [myRating, setMyRating] = useState(null as any);
     const [loading, setLoading] = useState(true);
@@ -43,26 +65,25 @@ const CardDetails: FC<PropsType> = ({marker, address, wasteLabels, deselect}) =>
         const getRatingFromApi = async () => {
             const data: any = await getRateAvarage(headers, marker.id);
             setInfoRating(data);
-        }
-        getRatingFromApi().catch((err) => (console.log(err)));
+        };
+        getRatingFromApi().catch((err) => console.log(err));
+    }, [myRating]);
 
+    useEffect(() => {
         const getMyRating = async () => {
             const data: any = await getRatingForCurrentUser(headers, marker.id);
             setMyRating(data);
-        }
-        getMyRating().catch((err) => (console.log(err)));
-
-    }, [getRateAvarage, getRatingForCurrentUser]);
+        };
+        getMyRating().catch((err) => console.log(err));
+    }, []);
 
     useEffect(() => {
         if (infoRating.idPoint != 0) {
-
             setLoading(false);
-        } 
-        else {
+        } else {
             setLoading(true);
         }
-    }, [infoRating])
+    }, [infoRating]);
 
     const headerDetails = () => (
         <View style={tw`flex-row justify-between items-center ml-6 pt-2`}>
@@ -79,85 +100,148 @@ const CardDetails: FC<PropsType> = ({marker, address, wasteLabels, deselect}) =>
     );
 
     const handleRateUp = () => {
-        if (myRating) {
-            if (myRating.rate !== 10) {
-                const rating = {...myRating, "rate": 10};
-                changeRating(headers, myRating.id, rating);
-                setMyRating(rating);
+        if (Date.now() > now + DELAY) {
+            now = Date.now();
+
+            if (myRating) {
+                if (myRating.rate !== 10) {
+                    const rating = { ...myRating, rate: 10 };
+                    const postChangeRating = async () => {
+                        const data = await changeRating(
+                            headers,
+                            myRating.id,
+                            JSON.stringify(rating)
+                        );
+                        if (data !== undefined) {
+                            setMyRating(rating);
+                        }
+                    };
+                    postChangeRating().catch((err) => console.log(err));
+                } else {
+                    deleteRating(headers, myRating.id);
+                    setMyRating(null);
+                }
             } else {
-                deleteRating(headers, myRating.id);
-                setMyRating(null);
+                const rating = {
+                    point: marker.id,
+                    user: authentication.user.id,
+                    rate: 10,
+                };
+
+                const postRating = async () => {
+                    const response_rating = await setRating(
+                        headers,
+                        JSON.stringify(rating)
+                    );
+                    if (response_rating !== undefined) {
+                        const data = await getRatingForCurrentUser(
+                            headers,
+                            marker.id
+                        );
+
+                        if (data !== undefined) {
+                            setMyRating(data);
+                        }
+                    }
+                };
+
+                postRating().catch((err) => console.log(err));
             }
-        } else {
-            const rating = {
-                "point": marker.id,
-                "user": authentication.user.id,
-                "rate": 10
-            };
-            setRating(headers, rating);
         }
-    }
+    };
 
     const handleRateDown = () => {
-        if (myRating) {
-            if (myRating.rate !== 0) {
-                const rating = {...myRating, "rate": 0};
-                changeRating(headers, myRating.id, rating);
-                setMyRating(rating);
+        if (Date.now() > now + DELAY) {
+            now = Date.now();
+            if (myRating) {
+                if (myRating.rate !== 0) {
+                    const rating = { ...myRating, rate: 0 };
+                    const postChangeRating = async () => {
+                        const data = await changeRating(
+                            headers,
+                            myRating.id,
+                            JSON.stringify(rating)
+                        );
+                        if (data !== undefined) {
+                            setMyRating(rating);
+                        }
+                    };
+
+                    postChangeRating().catch((err) => console.log(err));
+                } else {
+                    const postDeleteRating = async () => {
+                        await deleteRating(headers, myRating.id);
+                        setMyRating(null);
+                    };
+                    postDeleteRating().catch((err) => console.log(err));
+                }
             } else {
-                deleteRating(headers, myRating.id);
-                setMyRating(null);
-            }
-        } else {
-            const rating = {
-                "point": marker.id,
-                "user": authentication.user.id,
-                "rate": 0
-            };
-            setRating(headers, rating);
-        }
-    }
+                const rating = {
+                    point: marker.id,
+                    user: authentication.user.id,
+                    rate: 0,
+                };
 
-    const renderIconDown = () => {
-        if (myRating && myRating.rate < 5)
-            return (ArrowDownFillIcon)
-        else
-            return (ArrowDownIcon)
-    }
+                const postRating = async () => {
+                    const response_rating = await setRating(
+                        headers,
+                        JSON.stringify(rating)
+                    );
+                    if (response_rating !== undefined) {
+                        const data = await getRatingForCurrentUser(
+                            headers,
+                            marker.id
+                        );
 
-    const renderIconUp = () => {
-        if (myRating && myRating.rate > 5)
-            return (ArrowUpFillIcon)
-        else
-            return (ArrowUpIcon)
-    }
-
-    const footerDetails = () => (
-        (!loading ?
-            (<View style={tw`flex-row justify-around`}>
-                    <Button
-                        appearance="ghost"
-                        status="success"
-                        accessoryLeft={renderIconUp()}
-                        onPress={() => handleRateUp()}
-                    />
-                    {
-                        infoRating.idPoint !== 0 &&
-                        <Text>{infoRating.rate} / {infoRating.denominator}</Text>
+                        if (data !== undefined) {
+                            setMyRating(data);
+                        }
                     }
-                    <Button
-                        appearance="ghost"
-                        status="danger"
-                        accessoryLeft={renderIconDown()}
-                        onPress={() => handleRateDown()}
-                    />
-            </View>): (
-                    <View style={styles.container}>
-                        <Spinner/>
-                    </View>
-            )
-        )
-    );
+                };
+
+                postRating().catch((err) => console.log(err));
+            }
+        }
+    };
+
+    const footerDetails = () =>
+        !loading ? (
+            <View style={tw`flex-row justify-around`}>
+                <Button
+                    appearance="ghost"
+                    status="success"
+                    accessoryLeft={
+                        myRating?.rate > 5 ? (
+                            <ArrowUpFillIcon />
+                        ) : (
+                            <ArrowUpIcon />
+                        )
+                    }
+                    onPress={handleRateUp}
+                />
+                {infoRating.idPoint !== 0 && (
+                    <Text>
+                        {infoRating.rate} / {infoRating.denominator}
+                    </Text>
+                )}
+                <Button
+                    appearance="ghost"
+                    status="danger"
+                    accessoryLeft={
+                        myRating?.rate < 5 ? (
+                            <ArrowDownFillIcon />
+                        ) : (
+                            <ArrowDownIcon />
+                        )
+                    }
+                    onPress={handleRateDown}
+                />
+            </View>
+        ) : (
+            <View style={styles.container}>
+                <Spinner />
+            </View>
+        );
 
     return (
         <Card
@@ -181,7 +265,7 @@ const CardDetails: FC<PropsType> = ({marker, address, wasteLabels, deselect}) =>
 
 const styles = StyleSheet.create({
     cardDetail: tw`absolute w-5 bottom-0  w-full`,
-    container: tw`flex-row justify-around`
+    container: tw`flex-row justify-around`,
 });
 
 export default CardDetails;
